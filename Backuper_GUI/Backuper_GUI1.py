c7fec8710113b39d645d828b3c2333dd64543478
Derthar <dertgar159@gmail.com>
2023-06-26 23:41:24 +0500

    commit

--- Backuper_GUI/Backuper_GUI1.py
+++ Backuper_GUI/Backuper_GUI1.py
@@ -1,17 +1,18 @@
 import sys
+import threading
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIntValidator
-from PyQt5.QtCore import QThread
 import os
 import math
-# import time
-# import shutil
-# import datetime
-# from threading import Thread
+import time
+import shutil
+import datetime
 
 
 class DlgMain(QDialog):
     def __init__(self):
+        # variables
+        # region
         lenght = 800
         height = 600
         first_row = int(math.trunc(0.075 * height))
@@ -20,16 +21,22 @@ class DlgMain(QDialog):
         first_column = int(math.trunc(0.1 * lenght))
         between_column = int(math.trunc(0.6 * lenght))
         second_column = int(math.trunc(0.875 * lenght))
+        # endregion
+
         super().__init__()  # parent's __init__
         self.setWindowTitle("Backuper GUI")  # add window title
         self.setFixedSize(lenght, height)  # Main window size
 
         # set attributes
-        self.file_name = ''
-        self.bdir_path = ''
+        self.file_name = None
+        self.bdir_path = None
+        self.program_interval = None
+        self.save_interval = None
         self.validator = QIntValidator()
 
+
         # set text-labels
+        # region
         self.label_file = QLabel('Выберите необходимый файл:', self)
         self.label_file.move(int(0.4*lenght), int(0.025*height))
 
@@ -41,8 +48,10 @@ class DlgMain(QDialog):
 
         self.label_intsave = QLabel('Введите интервал сохранения файлов(в секундах)', self)
         self.label_intsave.move(int(between_column), int(0.26 * height))
+        # endregion
 
         # set text-lines
+        # region
         self.File_Text = QLineEdit('', self)  # add text line
         self.File_Text.move(first_column, first_row)  # move text line
         self.File_Text.setFixedSize(int(0.725*lenght), int(0.035*height))
@@ -65,8 +74,10 @@ class DlgMain(QDialog):
         self.terminal.move(int(0.1*lenght), int(0.4*height))
         self.terminal.setFixedSize(int(0.725*lenght), int(0.45*height))
         self.terminal.setReadOnly(True)
+        # endregion
 
         # set buttons
+        # region
         self.select_file_btn = QPushButton('Select file', self)
         self.select_file_btn.move(second_column, first_row)
         self.select_file_btn.clicked.connect(self.evt_select_file_btn_clicked)
@@ -77,11 +88,12 @@ class DlgMain(QDialog):
 
         self.start_btn = QPushButton('Get data', self)
         self.start_btn.move(int(0.3*lenght), int(0.9*height))
-        self.start_btn.clicked.connect(start_backup)
+        self.start_btn.clicked.connect(self.backuper)
 
-        self.cancel_btn = QPushButton("Cancel", self)
+        self.cancel_btn = QPushButton("Exit", self)
         self.cancel_btn.move(int(0.55*lenght), int(0.9*height))
         self.cancel_btn.clicked.connect(self.exit_func)
+        # endregion
 
         self.console_print('Инициализация прошла успешно. Приложение готово к работе!')
 
@@ -111,6 +123,8 @@ class DlgMain(QDialog):
         self.Int_Save_Text.setDisabled(True)
         self.select_file_btn.setDisabled(True)
         self.odir_btn.setDisabled(True)
+        self.File_Text.setDisabled(True)
+        self.Path_Text.setDisabled(True)
 
     def enabled_elements(self):
         self.File_Text.setDisabled(False)
@@ -120,66 +134,76 @@ class DlgMain(QDialog):
         self.select_file_btn.setDisabled(False)
         self.odir_btn.setDisabled(False)
 
-
-class MainWindow(QThread):
-    def __init__(self):
-        super().__init__()
-        start_window()
-
-
-class MainFunc(QThread):
-    def __init__(self):
-        super().__init__()
-
-    def run(self):
-        program.console_print('4')
-
-
-def check_values():
-    global program
-    if check_file_name() is True and check_bdir_path() is True:
-        program.disable_elements()
-        program.console_print('Проверка входных данных прошла успешно. Запуск программы...')
-
-
-def check_file_name():
-    program.console_print('Проверка входных данных')
-    if program.file_name is not None and os.path.exists(program.file_name):
-        program.console_print('Проверка параметра 1 прошла успешно')
-        return True
-    else:
-        program.console_print('Введите корректный путь до файла, который надо сохранять')
-        program.enabled_elements()
+    def check_values(self):
+        if self.check_file_name() and self.check_bdir_path() and self.check_program_interval() and self.check_program_int_save():
+            self.disable_elements()
+            self.console_print('Проверка входных данных прошла успешно. Запуск программы...')
+            return True
+        self.enabled_elements()
         return False
 
+    def check_file_name(self):
+        self.console_print('Проверка входных данных')
+        if self.file_name is not None and os.path.exists(self.file_name):
+            self.console_print('Проверка параметра 1 прошла успешно')
+            return True
+        else:
+            self.console_print('Введите корректный путь до файла, который надо сохранять')
+            self.enabled_elements()
+            return False
 
-def check_bdir_path():
-    if program.bdir_path is not None and os.path.exists(program.bdir_path):
-        program.console_print('Проверка параметра 2 прошла успешно')
-        return True
-    else:
-        program.console_print('Введите корректный путь до папки, в которую надо сохранять')
-        program.enabled_elements()
-        return False
+    def check_bdir_path(self):
+        if self.bdir_path is not None and os.path.exists(self.bdir_path):
+            self.console_print('Проверка параметра 2 прошла успешно')
+            return True
+        else:
+            self.console_print('Введите корректный путь до папки, в которую надо сохранять')
+            self.enabled_elements()
+            return False
+
+    def check_program_interval(self):
+        self.program_interval = self.Interval_Text.text()
+        if self.program_interval is not None:
+            try:
+                int(self.Interval_Text.text())
+            except:
+                self.console_print('Введите некорректный интервал сохранения')
+                self.enabled_elements()
+                return False
+            self.console_print('Проверка параметра 3 прошла успешно')
+            return True
+
+    def check_program_int_save(self):
+        self.save_interval = self.Int_Save_Text.text()
+        if self.save_interval is not None:
+            try:
+                int(self.save_interval)
+            except:
+                self.console_print('Введите корректный интервал удаления')
+                self.enabled_elements()
+                return False
+            self.console_print('Проверка параметра 4 прошла успешно')
+            return True
+    def backuper(self):
+        def inner():
+            if self.check_values():
+                self.console_print('Старт бэкапа')
+                self.console_print(threading.current_thread().name)
+                self.console_print(threading.main_thread().name)
+                i = 0
+                while i != 5:
+                    self.console_print(str(i))
+                    i += 1
+                    time.sleep(i)
+        threading.Thread(target=inner(), name='new').start()
 
 
-def backuper():
-    print('4')
 
 
-def start_window():
-    global program
+
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     program = DlgMain()
     program.show()
     sys.exit(app.exec_())
-
-
-def start_backup():
-    th_main = MainFunc()
-    th_main.start()
-
-
-if __name__ == '__main__':
-    main_window = MainWindow()
-    main_window.start()
